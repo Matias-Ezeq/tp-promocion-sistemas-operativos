b257402cc5aa049fb5487323c4dd8f568dfaba05
MayraGGarcia <mayragiselle.garcia@estudiantes.unahur.edu.ar>
2026-06-15 01:24:51 -0300

    TcpListener y manejo de hilos por cliente

--- socket-server/Program.cs
+++ socket-server/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Server
+{
+    static ConcurrentDictionary<string, TcpClient> clientes =
+        new ConcurrentDictionary<string, TcpClient>();
+
+    static void Main()
+    {
+        TcpListener servidor = new TcpListener(IPAddress.Any, 5000);
+        servidor.Start();
+
+        Console.WriteLine("Servidor iniciado. Esperando conexiones...");
+
+        while (true)
+        {
+            TcpClient cliente = servidor.AcceptTcpClient();
+            Console.WriteLine("Nueva conexión entrante...");
+
+            Thread hilo = new(() => ManejarCliente(cliente)) { IsBackground = true };
+            hilo.Start();
+        }
+    }
+
+    static void ManejarCliente(TcpClient cliente)
+    {
+        Console.WriteLine("Hilo iniciado para un cliente nuevo.");
+    }
+}
