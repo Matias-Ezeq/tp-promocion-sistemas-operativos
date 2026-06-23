bbdd303f4cb34321333a6c8fdb7c626abb73f858
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-06-23 16:16:50 -0300

    documentación del código

--- socket-client/Program.cs
+++ socket-client/Program.cs
@@ -6,22 +6,30 @@ class Client
     static void Main()
     {
         Globals.loggedIn = false;
+        //constantes de servidor, proximamente se pedirá al inicio de la ejecución
         const string serverIp = "127.0.0.1";
         const int serverPort = 5000;
         
+        //inicialización de conección TCP y stream de datos
         using var client = new TcpClient(serverIp, serverPort);
         using NetworkStream stream = client.GetStream();
+
+        //definición de lector y escritor del stream
         Globals.Reader = new(stream);
         Globals.Writer = new(stream) { AutoFlush = true };
         string message;
 
+        //inicialización del proceso responsable de recibir los mensajes del server
         Thread hiloReader = new(() => serverResponse()){IsBackground = true};
         hiloReader.Start();
 
+        //función auxiliar para definir el nick del usuario
         setNickname();
 
+        //pausa de ejecución hasta que se inicie sesión en el server
         System.Threading.SpinWait.SpinUntil( () => Globals.loggedIn );
 
+        //funcion de envío de mensaje hacia el servidor
         while(true){
             string output;
             message = Console.ReadLine().Trim();
@@ -40,6 +48,7 @@ class Client
 
     private static void setNickname()
     {
+        //recibe el nick por parte del usuario y lo envía al servidor, el cual se encarga de validarlo
         Console.Write("Ingrese su nickname: ");
         var nick = Console.ReadLine().Trim();
         Globals.Writer.WriteLine($"HELLO:{nick}");
@@ -47,6 +56,7 @@ class Client
 
     private static void serverResponse()
     {
+        //recibe el mensaje y lo envía a una funcion auxiliar que parsea la respuesta del servidor
         while(true){
             var response = Globals.Reader.ReadLine();
 
@@ -56,6 +66,7 @@ class Client
 
     private static void responseHandler(string response)
     {
+        //separa la respuesta en un array para obtener su tipo y contenido(s)
         var received = response.Split(":");
         string type = received[0];
         string content = received[1];
@@ -69,7 +80,8 @@ class Client
         {
              message = "";
         }
-
+        
+        //cambia el formato del mensaje recibido en base al tipo
         switch(type) 
         {
         case "OK":
@@ -97,9 +109,13 @@ class Client
 
 }
 
+//variables globales
 public static class Globals
 {
+    //flag de inicio de sesión
     public static bool loggedIn {get;set;}
+
+    //lector y escritor de stream
     public static StreamReader Reader {get;set;}
     public static StreamWriter Writer {get;set;}
 }
